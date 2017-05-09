667e8455c73d999ea6041e313f145c6bbf4db0b9
Fabian Orccon <fabian.orccon@pucp.pe>
2017-05-10 01:13:14 +0200

    DB: Actualizar datos de la base de datos

--- src/db/DBConexion.java
+++ src/db/DBConexion.java
@@ -18,9 +18,10 @@ import java.util.logging.Logger;
 public class DBConexion {
     private static DBConexion instancia;
     private Connection conexion;
-    private static final String SERVIDOR = "jdbc:mysql://localhost/mydb";
-    private static final String USUARIO = "root";
-    private static final String PASSWORD = "root";
+    private static final String SERVIDOR =
+            "jdbc:mysql://quilla.lab.inf.pucp.edu.pe/inf282g5";
+    private static final String USUARIO = "inf282g5";
+    private static final String PASSWORD = "qKHifUJIGh94Jh5Q";
     
     private DBConexion() {
         try {
