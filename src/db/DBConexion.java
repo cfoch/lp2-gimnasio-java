c90cba8abc14ee9736efb150a2629e9e51aa2818
Fabian Orccon <fabian.orccon@pucp.pe>
2017-06-14 00:25:59 +0200

    db: Colocar la instancia a null al cerrar la conexion

--- src/db/DBConexion.java
+++ src/db/DBConexion.java
@@ -53,6 +53,6 @@ public class DBConexion {
             Logger.getLogger(DBConexion.class.getName())
                     .log(Level.SEVERE, null, ex);
         }
-        this.conexion = null;
+        this.instancia = null;
     }
 }
